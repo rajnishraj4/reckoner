d04fd012d76205becaff0ff1573b032b6351b020
Rajnish Raj <rajnishraj4@gmail.com>
2024-02-19 23:55:37 +0530

    initial commit

---
+++ src/main/java/com/adira/reckoner/ReckonerApplication.java
@@ -0,0 +1,13 @@
+package com.adira.reckoner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReckonerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReckonerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adira/reckoner/ReckonerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adira.reckoner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReckonerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
